607fa49c01ea3de40c740190449b2df5d5562bba
mik-p <mppritchard3@hotmail.com>
2024-08-08 13:55:54 +0000

    base scheme class, bt scheme implementing, WIP

--- prompt_schemes/include/prompt_schemes/bt_scheme.hpp
+++ prompt_schemes/include/prompt_schemes/bt_scheme.hpp
@@ -1,10 +1,89 @@
 #pragma once
 
+#include <behaviortree_cpp/bt_factory.h>
+#include <tinyxml2.h>
+
+#include <prompt_schemes/scheme_base.hpp>
+#include <stdexcept>
 #include <string>
 #include <vector>
-// #include <behaviortree_cpp
 
-namespace prompt_schemes {
+namespace prompt_schemes
+{
+
+/**
+ * @brief XMLScrubber
+ *
+ * text to xml support for text that may contain errors and additional non-xml
+ * data
+ *
+ */
+class XMLScrubber
+{
+public:
+  XMLScrubber() = default;
+  ~XMLScrubber() = default;
+
+  // static functions for document scrubbing
+  static const std::string scrub_xml(const std::string& xml_str)
+  {
+    return XMLScrubber::remove_outer_data(XMLScrubber::fix_xml_syntax(xml_str));
+  }
+
+  // remove outer non-xml data
+  static const std::string remove_outer_data(const std::string& xml_str)
+  {
+    // find first root xml element
+    std::string::size_type start = xml_str.find("<root");
+
+    // find final root xml element
+    std::string::size_type end = xml_str.find("</root>");
+
+    // and return remaining xml
+    return xml_str.substr(start, end - start);
+  }
+
+  // find and repair xml syntax errors
+  static const std::string fix_xml_syntax(const std::string& xml_str)
+  {
+    // TODO: implement xml syntax error fixing
+    return xml_str;
+  }
+
+  // is there xml in the text
+  static const bool is_parseable(const std::string& xml_str)
+  {
+    // read string and find sufficient xml features to determine if it is
+    // parseable or not
+    return (xml_str.find("<root>") != std::string::npos);
+  }
+
+  // parse string to xml
+  static const tinyxml2::XMLDocument parse_xml(const std::string& xml_str)
+  {
+    // is the string parseable
+    if (!XMLScrubber::is_parseable(xml_str))
+    {
+      throw std::runtime_error("document does not contain parseable XML");
+    }
+
+    // scrub xml
+    std::string scrubbed_xml = XMLScrubber::scrub_xml(xml_str);
+
+    // parse string to xml
+    tinyxml2::XMLDocument doc;
+    tinyxml2::XMLError eResult = doc.Parse(xml_str.c_str());
+
+    // check for parse errors
+    if (eResult != tinyxml2::XML_SUCCESS)
+    {
+      throw std::runtime_error("failed to parse XML");
+    }
+
+    // return doc
+    return doc.ToDocument();
+  }
+};
 
 /**
  * @brief BTScheme
@@ -16,34 +95,77 @@ namespace prompt_schemes {
  * and a language model is used to guide the robot to achieve that goal
  *
  */
-class BTScheme {
- public:
-  // is there xml in the text
-  const bool is_parseable(const std::string& bt_xml) {
-    // read string and find sufficient xml features to determine if it is
-    // parseable or not
+class BTScheme : public SchemeBase
+{
+public:
+  // register robot action
+  const void register_action(const std::string& action)
+  {
   }
 
-  // parse string to xml
-  void parse_xml(const std::string& bt_xml) {
-    // parse string to xml
-
-    // store in bt_xml_
+public:
+  // parse xml to BT
+  const void parse_xml_to_bt(const std::string& bt_xml)
+  {
+    // parse xml string to BT
+    factory_.registerBehaviorTreeFromText(bt_xml);
   }
 
   // find the next part in the BT that is not clear enough for completion
   const std::string find_next_missing(const std::string& bt_xml);
 
- public:
-  // bt xml artifact
-  std::string bt_xml_;
+protected:
+  // implement the base class state functions
+  const bool starting(const std::string& doc_str) override
+  {
+    // skip this state
+    return true;
+  }
+
+  const bool collecting(const std::string& doc_str) override
+  {
+    // move to next state as the registered actions should be present
+    return true;
+  }
+
+  const bool negotiating(const std::string& doc_str) override
+  {
+    // try passing the xml
+    try
+    {
+      // parse the xml
+      tinyxml2::XMLDocument doc = XMLScrubber::parse_xml(doc_str);
+
+      // compare the xml to the registered actions
+      // and request more information if the actions are not found
+    }
+    catch (const std::runtime_error& e)
+    {
+      // return false if the xml is not parseable
+      return false;
+    }
+
+    return true;
+  }
+
+  const bool running(const std::string& doc_str) override
+  {
+    // scrub the xml
+    std::string scrubbed_xml = XMLScrubber::scrub_xml(doc_str);
+
+    // parse the xml to BT
+    parse_xml_to_bt(scrubbed_xml);
+
+    // skip this state
+    return true;
+  }
+
+private:
+  // bt factory
+  BT::BehaviorTreeFactory factory_;
 
   // registered actions to compare tree with
   std::vector<std::string> registered_actions_;
-
-  // valid tree elements to compare tree with
-  std::vector<std::string> valid_tree_elements_ = {
-      "action", "condition", "sequence", "selector", "parallel"};
 };
 
 }  // namespace prompt_schemes
---
+++ prompt_schemes/include/prompt_schemes/scheme_base.hpp
@@ -0,0 +1,190 @@
+#pragma once
+
+#include <string>
+
+namespace prompt_schemes
+{
+
+/**
+ * @brief SchemeBase
+ *
+ * Base class for prompt schemes that are used in the prompt bridge
+ * the class keeps track of the state of the scheme
+ * and a record of the document string that is being negotiated
+ * with the prompt provider
+ *
+ */
+class SchemeBase
+{
+public:
+  // scheme state type
+  /**
+   * @brief State
+   *
+   * An enum class representing the state of the prompt scheme. Each state
+   * represents a different phase of the scheme's operation.
+   *
+   * * `STARTING`: The scheme is starting.
+   * * `COLLECTING`: The scheme is actively collecting data from the robot for
+   * the prompt provider.
+   * * `NEGOTIATING`: The scheme has collected enough information to be able to
+   * negotiate with the prompt provider to finalise the an action document.
+   * * `RUNNING`: The scheme has finalised the document and is running it on
+   *               the robot.
+   * * `IDLE`: The scheme is not currently active.
+   */
+  enum class State
+  {
+    STARTING,
+    COLLECTING,
+    NEGOTIATING,
+    RUNNING,
+    IDLE
+  };
+
+public:
+  // cannot instantiate base class
+  SchemeBase() = delete;
+
+  ~SchemeBase() = default;
+
+  // get current state
+  const State state()
+  {
+    return state_;
+  }
+
+  // set document string
+  void set_doc_str(const std::string& doc_str)
+  {
+    doc_str_ = doc_str;
+  }
+
+  // get document string
+  const std::string get_doc_str()
+  {
+    return doc_str_;
+  }
+
+  // function to iterate through state actions and transitions
+  void tick(const std::string doc_str)
+  {
+    bool res = false;
+
+    switch (state_)
+    {
+      case State::STARTING:
+        res = starting(doc_str);
+        break;
+      case State::COLLECTING:
+        res = collecting(doc_str);
+        break;
+      case State::NEGOTIATING:
+        res = negotiating(doc_str);
+        break;
+      case State::RUNNING:
+        res = running(doc_str);
+        break;
+      case State::IDLE:
+        res = idle(doc_str);
+        break;
+    }
+
+    if (res)
+      return next();
+  }
+
+protected:
+  // virtual functions for each state
+  // actions in starting
+  // was started by external request
+  virtual const bool starting(const std::string& doc_str) = 0;
+  // actions in collecting
+  // collect request and list of available capabilities
+  // these can be registered using plugins, capability provider, or other
+  // explicit runtime registrations
+  virtual const bool collecting(const std::string& doc_str) = 0;
+  // actions in negotiating
+  // negotiate document with prompt provider
+  // this is the core of the prompt scheme
+  virtual const bool negotiating(const std::string& doc_str) = 0;
+  // actions in running
+  // create and manage lifecycle
+  // run the to completion or fail and return to negotiation
+  virtual const bool running(const std::string& doc_str) = 0;
+  // actions in idle
+  // nothing to do in idle except to listen for request
+  virtual const bool idle(const std::string& doc_str) = 0;
+
+  // transition to next state
+  void next()
+  {
+    switch (state_)
+    {
+      case State::STARTING:
+        collect();
+        break;
+      case State::COLLECTING:
+        negotiate();
+        break;
+      case State::NEGOTIATING:
+        run();
+        break;
+      case State::RUNNING:
+        idle();
+        break;
+      case State::IDLE:
+        start();
+        break;
+    }
+  }
+
+  // transition to previous state
+  // only really useful in the running state function
+  // to allow quick renegotiation
+  void retry()
+  {
+    switch (state_)
+    {
+      case State::RUNNING:
+        negotiate();
+        break;
+    }
+  }
+
+private:
+  // transition states
+  void start()
+  {
+    state_ = State::STARTING;
+  }
+
+  void collect()
+  {
+    state_ = State::COLLECTING;
+  }
+
+  void negotiate()
+  {
+    state_ = State::NEGOTIATING;
+  }
+
+  void run()
+  {
+    state_ = State::RUNNING;
+  }
+
+  void idle()
+  {
+    state_ = State::IDLE;
+  }
+
+private:
+  // current state
+  State state_ = State::IDLE;
+
+  // document string
+  std::string doc_str_;
+};
+
+}  // namespace prompt_schemes
